7285081633ec98d6b4ab87e3fc44d4555f9d1bcc
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-02-18 19:10:46 +0000

    button and burger menu done nicely and updated

--- main.js
+++ main.js
@@ -3,20 +3,18 @@ const navSlide = () => {
     const burger = document.getElementById('burger')
     const nav = document.getElementById('nav')
     const navLinks = document.querySelectorAll('.nav-links li')
-    const header = document.querySelector("header")
     
     burger.addEventListener('click', () => {
+        //prevents scrolling on mobile whist nav is open
+        document.body.classList.toggle('no-scroll')
         nav.classList.toggle('nav-active')
-        document.body.classList.toggle('fixed')
         navLinks.forEach( (link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
             } else {
                 link.style.animation = `navLinkFade 0.7s ease forwards ${index / 7 + 0.4}s`
                 }
-    })
-    burger.classList.toggle('toggle')
-    
+    }) 
   })
   
 }
