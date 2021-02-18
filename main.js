c92a83ce9a497d4a17ecbc37152c3f8cfb597444
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-02-18 11:56:44 +0000

    testing moblie scroll nav fix

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const navSlide = () => {
     const burger = document.getElementById('burger')
     const nav = document.getElementById('nav')
     const navLinks = document.querySelectorAll('.nav-links li')
+    const html = document.querySelector("html")
     
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active')
+            if (nav.getAttribute("class").indexOf('nav-active') > -1){
+                html.style.overflowY = 'hidden'
+            } else {
+                html.style.overflowY = 'visible'
+            }
         navLinks.forEach( (link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
@@ -14,7 +20,9 @@ const navSlide = () => {
                 }
     })
     burger.classList.toggle('toggle')
+    
   })
+  
 }
 
 navSlide()
